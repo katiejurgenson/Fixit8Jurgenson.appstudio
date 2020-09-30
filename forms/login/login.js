db167532c4ef2c860f38111d782379d93a04a370
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-09-30 16:06:56 -0500

    membership Form
    Completed the code requirements for the membership form

--- forms/login/login.js
+++ forms/login/login.js
@@ -1,4 +1,4 @@
-/*
+/* 
 let accountName = ""
 
 btnSubmit.onclick=function(){
--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,14 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+btnMemEnt.onclick=function(){
+  let inptName = inptFirstName.value
+  
+  if (members.includes(inptName)) {
+    lblMemMsg.value = inptName + " is a member!"
+  } else {
+    members.push(inptName);
+    lblMemMsg.value = inptName + " was not a member, but they are now!"
+    console.log(members)
+}
+}
